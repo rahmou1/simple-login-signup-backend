dc86278f47f4beabd04f03c3d84ab6adaa8a39d8
rahmou1 <ahmedrahmou11694@gmail.com>
2023-01-04 05:22:06 +0200

    adding db

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import helmet from 'helmet';
 import RateLimit from 'express-rate-limit';
 import config from './config';
 import errorMiddleware from './middleware/error.middleware';
-
+import db from './database';
 const PORT = config.port || 5000;
 // create an instance server
 const app: Application = express();
@@ -37,6 +37,20 @@ app.post('/', (req: Request, res: Response) => {
     data: req.body
   });
 });
+
+//* testing database
+// db.connect().then((client) => {
+//   return client
+//     .query('SELECT NOW()')
+//     .then((res) => {
+//       client.release();
+//       console.log(res.rows);
+//     })
+//     .catch((err) => {
+//       client.release();
+//       console.log(err.stack);
+//     });
+// });
 
 // handling any error on the server
 app.use(errorMiddleware);
