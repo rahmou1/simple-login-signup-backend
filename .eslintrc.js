dc8e2c71b0bda849c420f69caa8bb0ec4698c3fa
rahmou1 <ahmedrahmou11694@gmail.com>
2023-01-04 04:21:20 +0200

    adding error handler across all project

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
     node: true,
     jest: true
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 13,
@@ -12,11 +16,10 @@ module.exports = {
   },
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    semi: ['error', 'never'],
     'no-use-before-define': ['error', { functions: true, classes: true }],
-    'prettier/prettier': 2, // Means error
+    'prettier/prettier': 0, // Means error
     'no-console': 1, // Means warning
-    'no-var': 'error',
-    'prefer-const': 'error'
+    'no-var': 'never',
+    'prefer-const': 'never'
   }
-}
+};
--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,59 @@
-import express, { Application, Request, Response } from 'express'
-import morgan from 'morgan'
-import helmet from 'helmet'
-import rateLimit from 'express-rate-limit'
-import * as dotenv from 'dotenv'
+import express, { Application, Request, Response } from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import RateLimit from 'express-rate-limit';
+import * as dotenv from 'dotenv';
+import errorMiddleware from './middleware/error.middleware';
 
-dotenv.config()
+dotenv.config();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 // create an instance server
-const app: Application = express()
+const app: Application = express();
 // middleware to parse incoming requests
-app.use(express.json())
+app.use(express.json());
 // HTTP request logger middleware
-app.use(morgan('common'))
+app.use(morgan('common'));
 // HTTP security middleware
-app.use(helmet())
+app.use(helmet());
 /// Apply the rate limiting middleware to all requests
 app.use(
-  rateLimit({
+  RateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     message: 'Too many requests from this IP, please try again later'
   })
-)
+);
 // add routing for / path
 app.get('/', (req: Request, res: Response) => {
   res.json({
     message: 'Hello World 🌍'
-  })
-})
+  });
+});
 
 app.post('/', (req: Request, res: Response) => {
   res.json({
     message: 'Hello World 🌍 from post',
     data: req.body
-  })
-})
+  });
+});
+
+// handling any error on the server
+app.use(errorMiddleware);
+
+// handling global errors
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    message:
+      'Oh you are lost, Read the API documentation to find your way back home 😂'
+  });
+});
 
 // start express server
 app.listen(PORT, () => {
-  console.log(`Server is starting at port:${PORT}`)
-})
+  console.log(`Server is starting at port:${PORT}`);
+});
 
-export default app
+export default app;
